3120b43a6360b1f233b59c0bb700af1a05f287a7
John Waczak <john.louis.waczak@gmail.com>
2024-03-25 09:41:58 -0500

    updated with camera struct and anti-alliasing

---
+++ src/hit.rs
@@ -0,0 +1,50 @@
+use super::vec::{Vec3, Point3};
+use super::ray::Ray;
+
+
+/// A struct storing data for ray-object intersection
+pub struct HitRecord{
+    pub p: Point3,      // intersection point
+    pub normal: Vec3,   // outward surface normal
+    pub t: f64,         // intersection "time"
+    pub front_face: bool,
+}
+
+
+/// implementation for HitRecord
+impl HitRecord {
+    pub fn set_face_normal(&mut self, r: &Ray, outward_normal: Vec3) -> () {
+        self.front_face = r.direction().dot(outward_normal) < 0.0;
+        self.normal = if self.front_face {
+            outward_normal
+        } else {
+            (-1.0) * outward_normal
+        };
+    }
+}
+
+
+/// A trait for all "hitabale" objects
+pub trait Hit {
+    fn hit(&self, r: &Ray, t_min: f64, t_max: f64) -> Option<HitRecord>;
+}
+
+/// A dynamic list of objects implementing the "Hit" trait
+pub type World = Vec<Box<dyn Hit>>;
+
+impl Hit for World {
+    fn hit(&self, r: &Ray, t_min: f64, t_max: f64) -> Option<HitRecord> {
+        let mut tmp_rec = None;
+        let mut closest_so_far = t_max;
+
+        for object in self {
+            if let Some(rec) = object.hit(r, t_min, closest_so_far) {
+                closest_so_far = rec.t;
+                tmp_rec = Some(rec);
+            }
+        }
+
+        tmp_rec
+    }
+}
+
--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,32 @@
 mod vec;
 mod ray;
+mod hit;
+mod sphere;
+mod camera;
 
 use vec::{Vec3, Point3, Color};
 use ray::Ray;
+use hit::{Hit, World};
+use sphere::Sphere;
+use camera::Camera;
+use rand::Rng;
+
 use std::io::{stderr, Write};
 
-
-/// check if ray has hit sphere
-fn hit_sphere(center: Point3, radius: f64, r: &Ray) -> f64 {
-    let oc = r.origin() - center;
-
-    let a: f64 = r.direction().dot(r.direction());
-    let b: f64 = 2.0 * oc.dot(r.direction());
-    let c: f64 = oc.dot(oc) - radius.powi(2);
-    let discriminant = b.powi(2) - 4.0 * a * c;
-
-    if discriminant < 0.0 {
-        -1.0
-    } else {
-        (-b - discriminant.sqrt()) / (2.0 * a)
-    }
-}
 
 
 
 /// return background color
-fn ray_color(r: &Ray) -> Color {
-    // check intersection with sphere of radius 0.5 at z=-1.0
-    let t = hit_sphere(Point3::new(0.0, 0.0, -1.0), 0.5, r);
-
-    if t > 0.0 {
-        let n = (r.at(t) - Point3::new(0.0, 0.0, -1.0)).normalized();
-        return 0.5 * Color::new(n.x() + 1.0, n.y() + 1.0, n.z() + 1.0);
+fn ray_color(r: &Ray, world: &World) -> Color {
+    if let Some(rec)  = world.hit(r, 0.0, f64::INFINITY) {
+        // if intersection, color as sphere
+        0.5 * (rec.normal + Color::new(1.0, 1.0, 1.0))
+    } else {
+        // if no intersection, color as background
+        let unit_direction = r.direction().normalized();
+        let t = 0.5 * (unit_direction.y() + 1.0);
+        (1.0 -t) * Color::new(1.0, 1.0, 1.0) + t * Color::new(0.5, 0.7, 1.0)
     }
-
-    let unit_direction = r.direction().normalized();
-
-    // convert ray height from [-1,1] --> [0, 1]
-    let t = 0.5 * (unit_direction.y() + 1.0);
-
-    // interpolate between blue and white
-    (1.0 - t) * Color::new(1.0, 1.0, 1.0) + t * Color::new(0.5, 0.7, 1.0)
 }
 
 
@@ -49,23 +35,22 @@ fn main() {
     const ASPECT_RATIO: f64 = 16.0 / 9.0;
     const IMAGE_WIDTH: u64 = 256;
     const IMAGE_HEIGHT: u64 = ((IMAGE_WIDTH as f64) / ASPECT_RATIO) as u64;
+    const SAMPLES_PER_PIXEL: u64 = 100;
 
+    // WORLD
+    let mut world = World::new();
+    world.push(Box::new(Sphere::new(Point3::new(0.0, 0.0, -1.0), 0.5)));
+    world.push(Box::new(Sphere::new(Point3::new(0.0, -100.5, -1.0), 100.0)));
 
     // CAMERA
-    let viewport_height = 2.0;
-    let viewport_width = ASPECT_RATIO * viewport_height;
-    let focal_length = 1.0; // distance from viewport to camera
-
-    // x, y, z
-    let origin = Point3::new(0.0, 0.0, 0.0);
-    let horizontal = Vec3::new(viewport_width, 0.0, 0.0);
-    let vertical = Vec3::new(0.0, viewport_height, 0.0);
-    let lower_left_corner = origin - (horizontal / 2.0) - (vertical / 2.0) - Vec3::new(0.0, 0.0, focal_length);
-
+    let cam = Camera::new();
 
     println!("P3");
     println!("{} {}", IMAGE_WIDTH, IMAGE_HEIGHT);
     println!("255"); // rgb values in [0, 255]
+
+    let mut rng = rand::thread_rng();
+
 
     // rows are written out
     for j in (0..IMAGE_HEIGHT).rev() {
@@ -74,17 +59,19 @@ fn main() {
 
         // pixels are written as rows from left to right
         for i in 0..IMAGE_WIDTH {
-            let u = (i as f64) / ((IMAGE_WIDTH - 1) as f64);
-            let v = (j as f64) / ((IMAGE_HEIGHT - 1) as f64);
+            let mut pixel_color = Color::new(0.0, 0.0, 0.0);
+            for _ in 0..SAMPLES_PER_PIXEL {
+                let random_u: f64 = rng.gen();
+                let random_v: f64 = rng.gen();
 
-            let r = Ray::new(
-                origin,
-                lower_left_corner + (u*horizontal) + (v * vertical) - origin,
-            );
+                let u = ((i as f64) + random_u) / ((IMAGE_WIDTH - 1) as f64);
+                let v = ((j as f64) + random_v) / ((IMAGE_HEIGHT - 1) as f64);
 
-            let pixel_color = ray_color(&r);
+                let r = cam.get_ray(u, v);
+                pixel_color += ray_color(&r, &world);
+            }
 
-            println!("{}", pixel_color.format_color());
+            println!("{}", pixel_color.format_color(SAMPLES_PER_PIXEL));
         }
     }
 
